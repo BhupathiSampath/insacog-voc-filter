0e3121d8e3d39759f16ba07c809fde486c6b47ab
BhupathiSampath <samastha849@gmail.com>
2022-01-24 10:36:51 +0530

    main commits

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="md:flex space-x-10">
+    <nav class="md:flex space-x-10 bg-gray-800">
     <div class="max-w-full mx-auto">
       <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <a class="text-base font-medium text-gray-500 hover:text-gray-900">
--- pages/stackedbar.vue
+++ pages/stackedbar.vue
@@ -1,225 +1,111 @@
 <template>
-    <div>
-    <div class="barChart col-md-6">
-      <bar-chart :chart-data="barchartDatacollection" :height="500" :width="2000" style="display: block; width: 1500px; height: 384px;"></bar-chart>
-      </div>
-  </div>
+<div class="box-content">
+  <section>
+    <div class="container mx-auto">
+      <!-- <select class="shadow appearance-none border rounded w-50 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer" v-on:keyup.enter="fetchSomething()" v-model="year" name="year">
+        <option value="202" name="year">Both 2020 && 2021</option>
+        <option value="2020">2020</option>
+        <option value="2021" >2021</option>
+      </select> -->
+      <!-- <button  class="bg-blue-500 hover:bg-blue-700 content-left text-white font-bold py-2 px-4 rounded" v-on:click="fetchSomething">Get Graph</button> -->
+      <BarChart :key="random" :data="barChartData" :options="barChartOptions" :height="500" :width="2000" style="display: block; width: 1500px; height: 384px;"></BarChart>
+    </div>
+  </section>
+</div>
 </template>
 
 <script>
-import BarChart from "/home/nsm-07/Desktop/Bhupati/dev/insacog-voc-filter/components/Charts/StackedBarChart.vue";
-import axios from "axios"
+import axios from 'axios'
+import BarChart from "/home/nsm-07/Desktop/Bhupati/insacog-voc-filter/components/Charts/StackedBarChart.vue";
+import moment from 'moment'
 export default {
-  components: {
-    BarChart
-  },
-  data() {
-    
+  components: { BarChart },
+  
+  data () {
     return {
-      arrLineage: [],
-      arrCount: [],
-      arrMonth: [],
-      barchartDatacollection: { labels: [], datasets: [], options: {} }, 
-     
-    };
+      year: "202",
+      random: 123456,
+      arrMonthNumber: [],
+      arrMonthdata: [],
+      barChartData: {
+    labels: [
+      // moment(new Date(2020, 0, 1)).format('YYYY-MM-DD'),
+      // moment(new Date(2020, 0, 2)).format('YYYY-MM-DD'),
+      // moment(new Date(2020, 0, 3)).format('YYYY-MM-DD')
+    ],
+    datasets: [{
+        label: '# of Green Votes',
+        data: [12, 19, 13],
+        borderWidth: 1,
+        backgroundColor: ['green', 'green', 'green']
+      },
+      {
+        label: '# of Pink Votes',
+        data: [15, 9, 19],
+        borderWidth: 1,
+        backgroundColor: ['pink', 'pink', 'pink']
+      }
+    ]
+  },
+  barChartOptions: {
+    scales: {
+      xAxes: [{
+        stacked: true,
+        gridLines: {
+                display: false,
+        }
+      }],
+      yAxes: [{
+        stacked: true
+      }]
+    }
+  }
+    } 
   },
   async created() {
-    const { data } = await axios.get(`${process.env.baseUrl}/stackbar/`);
-    console.log(data.slice(0,))
-    console.log(Object.values(data))
-    // console.log(Object.values(data)[0].slice(0,).lineage)
-    // console.log(Object.values(data)[0])
-    // const jan = Object.values(data)
-    const brands = Object.values(data).map(({ lineage }) => lineage);
-    console.log(brands)
+    const { data } = await axios.get(`${process.env.baseUrl}/stackbar/?year=${this.year}`);
+    // Object.entries(data).forEach(([value]) => {
+    //   console.log(value);
+    //   });
+    const values = Object.values(data)
+    console.log(values)
+    console.log(Object.keys(values))
+    console.log(Object.values(data).keys(Object.values(data)))
+    // console.log(Object.keys(data).map((key) => [Number(key), data[key]]))
     data.forEach(d => {
       const {
-        lineage,
-        strain__count
+        month_number,
+        // ["B.1.1.306"],
+        B,
+        
       } = d;
-      this.arrCount.push(strain__count)
-      this.arrLineage.push(lineage)
+      this.arrMonthdata.push(B)
+      this.arrMonthNumber.push(month_number)
     });
-    console.log(this.arrLineage)
-    console.log(this.arrCount)
-    this.arrMonth.push(Object.keys(data))
-    this.barchartDatacollection.labels = this.arrMonth
-    console.log(this.barchartDatacollection.labels)
-    
-    // this.barChartData.datasets[0].data = this.arrweekdata
-    // this.random = 456789
+    console.log(this.arrMonthNumber)
+    // console.log(this.arrMonthdata)
+    this.barChartData.labels = this.arrMonthNumber
+    // this.barChartData.datasets[0].data = this.arrMonthdata
+    this.random = 456789
       
   },
-  mounted() {
-    this.barchartdata();
-  },
-  methods: {
-    barchartdata() {
-      this.barchartDatacollection = {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-
-        datasets: [
-          {
-            label: "Bad Style",
-            data: [40, 47, 44, 38, 27,40, 47, 44, 38, 27,40, 47],
-            backgroundColor: "#512DA8",
-            hoverBackgroundColor: "#7E57C2",
-            hoverBorderWidth: 0
-          },
-          // {
-          //   label: "Warning",
-          //   data: [10, 12, 7, 5, 4,12, 7, 5, 4,12, 7, 5],
-          //   backgroundColor: "#FFA000",
-          //   hoverBackgroundColor: "#FFCA28",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12,17, 11, 22, 18, 12,17, 11, 22],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Bad Style",
-          //   data: [40, 47, 44, 38, 27,17, 11, 22, 18, 12,17, 11, 22],
-          //   backgroundColor: "#512DA8",
-          //   hoverBackgroundColor: "#7E57C2",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Warning",
-          //   data: [10, 12, 7, 5, 4],
-          //   backgroundColor: "#FFA000",
-          //   hoverBackgroundColor: "#FFCA28",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Bad Style",
-          //   data: [40, 47, 44, 38, 27],
-          //   backgroundColor: "#512DA8",
-          //   hoverBackgroundColor: "#7E57C2",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Warning",
-          //   data: [10, 12, 7, 5, 4],
-          //   backgroundColor: "#FFA000",
-          //   hoverBackgroundColor: "#FFCA28",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Bad Style",
-          //   data: [40, 47, 44, 38, 27],
-          //   backgroundColor: "#512DA8",
-          //   hoverBackgroundColor: "#7E57C2",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Warning",
-          //   data: [10, 12, 7, 5, 4],
-          //   backgroundColor: "#FFA000",
-          //   hoverBackgroundColor: "#FFCA28",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Bad Style",
-          //   data: [40, 47, 44, 38, 27],
-          //   backgroundColor: "#512DA8",
-          //   hoverBackgroundColor: "#7E57C2",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Warning",
-          //   data: [10, 12, 7, 5, 4],
-          //   backgroundColor: "#FFA000",
-          //   hoverBackgroundColor: "#FFCA28",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // },
-          // {
-          //   label: "Error",
-          //   data: [17, 11, 22, 18, 12],
-          //   backgroundColor: "#D32F2F",
-          //   hoverBackgroundColor: "#EF5350",
-          //   hoverBorderWidth: 0
-          // }
-        ],
-        options: {
-          scales: {
-            xAxes: [
-              {
-                stacked: true,
-                // gridLines: { display: true }
-              }
-            ],
-            yAxes: [
-              {
-                stacked: true
-              }
-            ]
-          },
-          legend: { display: true }
-        }
-      };
-    }
-  },
   
-};
+  // methods: {
+  //   async fetchSomething() {
+  //   const { data } = await axios.get(`${process.env.baseUrl}/monthlydistribution/?year=${this.year}`);
+  //   data.forEach(d => {
+  //     const {
+  //       month_number,
+  //       strain__count
+  //     } = d;
+  //     this.arrMonthdata.push(strain__count)
+  //     this.arrMonthNumber.push(month_number)
+  //   });
+  //   this.barChartData.labels = this.arrMonthNumber
+  //   this.barChartData.datasets[0].data = this.arrMonthdata
+  //   this.random = 456789
+  //   }
+  // }
+  }
 
-  
 </script>
