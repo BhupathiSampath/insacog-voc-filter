1f01096e9af85a2b0bc2fa6dc1986825c6a7026c
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-02-19 13:23:19 +0530

    updated file - components/NuxtLogo.vue

--- components/NuxtLogo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <img class="h-8 w-auto sm:h-10" src="/home/nsm-07/Desktop/Bhupati/insacog-voc-filter/dist/INSACOG_Logo2.png" alt="">
-</template>
-
-<style>
-.nuxt-logo {
-  height: 180px;
-}
-</style>
