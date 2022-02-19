faae556d78b82d36e29bddc5cf21642cd6912a0b
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-02-19 14:53:28 +0530

    updated file - store/base.js

--- store/base.js
+++ store/base.js
@@ -2,6 +2,10 @@ import { getField, updateField } from 'vuex-map-fields'
 
 export const state = () => ({
 	datahub_stats: {},
+	lineage_class: {
+		chartdata: {},
+		loaded: false
+	},
 	datahub_loaded: false,
 })
 
@@ -14,6 +18,10 @@ export const mutations = {
 		state.datahub_stats = payload
 		state.datahub_loaded = true
 	},
+	SET_LINEAGE_CLASSIFICATION(state, payload) {
+		state.lineage_class.chartdata = payload
+		state.lineage_class.loaded  = true
+	},
 	updateField,
 }
 
@@ -21,5 +29,9 @@ export const actions = {
 	async CallINSACOGDataHubStats({ commit, dispatch }) {
 		const response = await this.$axios.$post('https://research.nibmg.ac.in/insacog/api/files/landing-stats/')
 		await commit('SET_DATAHUB_STATS', response)
+	},
+	async CallLineageClassification({ commit, dispatch }) {
+		const response = await this.$axios.$get('http://10.10.6.87/api/linclassification/')
+		await commit('SET_LINEAGE_CLASSIFICATION', response)
 	},
 }
