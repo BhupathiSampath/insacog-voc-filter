074f4f71ad024c74732bee3a426d492525707f15
BhupathiSampath <samastha849@gmail.com>
2022-02-23 14:23:50 +0530

    pie chart

--- components/charts/BarChart.vue
+++ components/charts/BarChart.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-chart class="chart" :option="option" />
+	<v-chart class="chart" :loading="chart_loader" :loading-options="loader_option" :option="option" />
 </template>
 
 <script>
@@ -14,48 +14,68 @@ use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent
 
 export default {
 	data: () => ({
-		option: {
-			title: {
-				text: 'Traffic Sources',
-				left: 'center',
-			},
-			tooltip: {
-				trigger: 'item',
-			},
-			legend: {
-				orient: 'vertical',
-				left: 'left',
-				data: [],
-			},
-			series: [
-				{
-					name: 'Traffic Sources',
-					type: 'pie',
-					radius: '55%',
-					center: ['50%', '60%'],
-					data: [],
-					emphasis: {
-						itemStyle: {
-							shadowBlur: 10,
-							shadowOffsetX: 0,
-							shadowColor: 'rgba(0, 0, 0, 0.5)',
-						},
-					},
-				},
-			],
+		chart_loader: true,
+		loader_option: {
+			lineWidth: 3,
+			fontSize: 14,
+			text: 'Loading',
+			fontWeight: 500,
+			color: '#c23531',
+			maskColor: '#f6f8f9',
+			fontFamily: 'Averta',
 		},
+		option : {
+		  tooltip: {
+		    trigger: 'item',
+		    formatter: '{a} <br/>{b} : {c} ({d}%)'
+		  },
+		  legend: {
+		    top: '5%',
+		    left: 'center'
+		  },
+		  series: [
+		    {
+		      name: 'Access From',
+		      type: 'pie',
+		      radius: ['40%', '70%'],
+		      avoidLabelOverlap: false,
+		      itemStyle: {
+		        borderRadius: 10,
+		        borderColor: 'rgb(245, 245, 245)',
+		        borderWidth: 2
+		      },
+		      label: {
+		        show: false,
+		        position: 'center'
+		      },
+		      emphasis: {
+		        label: {
+		          show: true,
+		          fontSize: '40',
+		          fontWeight: 'bold'
+		        }
+		      },
+		      labelLine: {
+		        show: false
+		      },
+		      data: [
+		      ]
+		    }
+		  ]
+		}
 	}),
 	components: {
 		VChart,
 	},
 	provide: {
-		[THEME_KEY]: 'dark',
+		[THEME_KEY]: 'light',
 	},
 	watch: {
 		chartdata(value) {
 			let temp = value.map((d) => ({ name: d.Class, value: d.strain__count }))
-			this.option.legend.data = temp.map((d) => d.name)
+			// this.option.legend.data = temp.map((d) => d.name)
 			this.option.series[0].data = temp
+		this.chart_loader = false
 		},
 	},
 	computed: { ...mapFields('base', ['lineage_class.chartdata', 'lineage_class.loaded']) },
